d74ba7cc9f15dbca6109c832c2d27ca76824c82e
Herbert Zurita <herbo351@gmail.com>
2021-10-23 18:52:31 -0500

    misc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { ServiceMiniCardComponent } from './service-mini-card/service-mini-card.component';
+import { ServiceFullCardComponent } from './service-full-card/service-full-card.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    ServiceMiniCardComponent,
+    ServiceFullCardComponent,
+  ],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
