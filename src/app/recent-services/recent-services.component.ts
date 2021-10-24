ca16c9734c56f59f877546926da9b50cffb77c6c
Herbert Zurita <herbo351@gmail.com>
2021-10-23 19:57:39 -0500

    recent services

---
+++ src/app/recent-services/recent-services.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-services',
+  templateUrl: './recent-services.component.html',
+  styleUrls: ['./recent-services.component.scss'],
+})
+export class RecentServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
